4b9c878817dd32b4451d4cda678ddf572f02987b
iria <iria>
2023-04-27 02:17:04 +0200

    Primera web. PEC.1. de la asignatura Herramientas

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const validator = require('validator');
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  if (validator.isEmpty(name)) {
+    alert('Por favor, ingresa tu nombre.');
+    return;
+  }
+
+  if (!validator.isEmail(email)) {
+    alert('Por favor, ingresa un correo electrónico válido.');
+    return;
+  }
+
+  if (validator.isEmpty(message)) {
+    alert('Por favor, ingresa tu mensaje.');
+    return;
+  }
+
+  alert('Formulario enviado con éxito.');
+});
+
+
+
+
+console.log('¡Hola, aves!');
+
