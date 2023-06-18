5374c2d5bdd73f940b86a41a284adf47bc84bd68
iria <iria>
2023-06-19 00:46:36 +0200

    Mensaje de confirmacion

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,33 @@
+// Función para compartir en Facebook
+function compartirFacebook() {
+  var url = encodeURIComponent(window.location.href);
+  window.open('https://www.facebook.com/sharer/sharer.php?u=' + url, '_blank');
+}
+
+// Función para compartir en Twitter
+function compartirTwitter() {
+  var url = encodeURIComponent(window.location.href);
+  var texto = encodeURIComponent('¡Echa un vistazo a esta página!');
+  window.open('https://twitter.com/intent/tweet?url=' + url + '&text=' + texto, '_blank');
+}
+
+// Función para compartir en LinkedIn
+function compartirLinkedIn() {
+  var url = encodeURIComponent(window.location.href);
+  var titulo = encodeURIComponent('Título de la página');
+  var resumen = encodeURIComponent('Breve descripción de la página');
+  window.open('https://www.linkedin.com/shareArticle?url=' + url + '&title=' + titulo + '&summary=' + resumen, '_blank');
+}
+
+// Asignar eventos de clic a los botones de compartir
+document.getElementById('facebookBtn').addEventListener('click', compartirFacebook);
+document.getElementById('twitterBtn').addEventListener('click', compartirTwitter);
+document.getElementById('linkedinBtn').addEventListener('click', compartirLinkedIn);
 
 
 
 
 
 
-
-const validator = require('validator');
-const contactForm = document.getElementById('contact-form');
-
-contactForm.addEventListener('submit', function (event) {
-  event.preventDefault(); // Agregamos esta línea para evitar que la página se refresque
-
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const message = document.getElementById('message').value;
-
-  if (validator.isEmpty(name)) {
-    alert('Por favor, ingresa tu nombre.');
-    return;
-  }
-
-  if (!validator.isEmail(email)) {
-    alert('Por favor, ingresa un correo electrónico válido.');
-    return;
-  }
-
-  if (validator.isEmpty(message)) {
-    alert('Por favor, ingresa tu mensaje.');
-    return;
-  }
-
-  alert('Formulario enviado con éxito.');
-});
 
 console.log('¡Hola, aves!');
---
+++ src/js/scripts.js
@@ -0,0 +1,26 @@
+// Función para compartir en Facebook
+function compartirFacebook() {
+    var url = encodeURIComponent(window.location.href);
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + url, '_blank');
+  }
+  
+  // Función para compartir en Twitter
+  function compartirTwitter() {
+    var url = encodeURIComponent(window.location.href);
+    var texto = encodeURIComponent('¡Echa un vistazo a esta página!');
+    window.open('https://twitter.com/intent/tweet?url=' + url + '&text=' + texto, '_blank');
+  }
+  
+  // Función para compartir en LinkedIn
+  function compartirLinkedIn() {
+    var url = encodeURIComponent(window.location.href);
+    var titulo = encodeURIComponent('Título de la página');
+    var resumen = encodeURIComponent('Breve descripción de la página');
+    window.open('https://www.linkedin.com/shareArticle?url=' + url + '&title=' + titulo + '&summary=' + resumen, '_blank');
+  }
+  
+  // Asignar eventos de clic a los botones de compartir
+  document.getElementById('facebookBtn').addEventListener('click', compartirFacebook);
+  document.getElementById('twitterBtn').addEventListener('click', compartirTwitter);
+  document.getElementById('linkedinBtn').addEventListener('click', compartirLinkedIn);
+  
